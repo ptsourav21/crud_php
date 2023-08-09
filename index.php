b858f881c7537d8db5e7c9687600f2d4c0f37207
Purnendu Talukder <ptsourav21@gmail.com>
2023-08-10 02:00:06 +0600

    Added php code in index for  dynamically show the data from database

--- index.php
+++ index.php
@@ -3,6 +3,13 @@ include 'header.php';
 ?>
 <div id="main-content">
     <h2>All Records</h2>
+    <?php
+        $conn=mysqli_connect("localhost", "root", "", "crud_php") or die("Connection Failed");
+        $sql=  "SELECT * FROM student JOIN studentclass WHERE student.sclass= studentclass.cid";
+        $result = mysqli_query($conn, $sql) or die("Query Unsuccessfull");
+
+        if(mysqli_num_rows($result)>0) {
+    ?>
     <table cellpadding="7px">
         <thead>
         <th>Id</th>
@@ -13,63 +20,30 @@ include 'header.php';
         <th>Action</th>
         </thead>
         <tbody>
+            <?php
+                while($row = mysqli_fetch_assoc($result)){;
+            ?>
             <tr>
-                <td>1</td>
-                <td>Ramesh</td>
-                <td>Delhi</td>
-                <td>BCA</td>
-                <td>9876543210</td>
+                <td><?php echo $row['sid'];?></td>
+                <td><?php echo $row['sname'];?></td>
+                <td><?php echo $row['saddress'];?></td>
+                <td><?php echo $row['cname'];?></td>
+                <td><?php echo $row['sphone'];?></td>
                 <td>
                     <a href='edit.php'>Edit</a>
                     <a href='delete-inline.php'>Delete</a>
                 </td>
             </tr>
-            <tr>
-                <td>2</td>
-                <td>Suresh</td>
-                <td>Punjab</td>
-                <td>BCOM</td>
-                <td>9876543210</td>
-                <td>
-                    <a href='edit.php'>Edit</a>
-                    <a href='delete-inline.php'>Delete</a>
-                </td>
-            </tr>
-            <tr>
-                <td>3</td>
-                <td>Suresh</td>
-                <td>Haryana</td>
-                <td>BSC</td>
-                <td>9876543210</td>
-                <td>
-                    <a href='edit.php'>Edit</a>
-                    <a href='delete-inline.php'>Delete</a>
-                </td>
-            </tr>
-            <tr>
-                <td>4</td>
-                <td>Krishna</td>
-                <td>Gujrat</td>
-                <td>BCA</td>
-                <td>9876543210</td>
-                <td>
-                    <a href='edit.php'>Edit</a>
-                    <a href='delete-inline.php'>Delete</a>
-                </td>
-            </tr>
-            <tr>
-                <td>5</td>
-                <td>Rohit</td>
-                <td>Delhi</td>
-                <td>BCA</td>
-                <td>9876543210</td>
-                <td>
-                    <a href='edit.php'>Edit</a>
-                    <a href='delete-inline.php'>Delete</a>
-                </td>
-            </tr>
+            
         </tbody>
+        <?php } ?>
     </table>
+    <?php } else{
+        echo "<h3>No Record Found</h3>";
+    }
+    mysqli_close($conn);
+    ?>
+
 </div>
 </div>
 </body>
